f28db0e30db84d36f540c37e7cc796fd21e84000
Andreas Dieckow <andreasdieckow@Andreas-MacBook-Pro.local>
2015-01-12 14:52:48 -0500

    rev test

--- PostCard/ViewController.swift
+++ PostCard/ViewController.swift
@@ -29,7 +29,7 @@ class ViewController: UIViewController {
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
-        messageLabel.textColor = UIColor.redColor()
+        messageLabel.textColor = UIColor.redColor() //changes the color to red
         
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder() //makes they keyboard go away
